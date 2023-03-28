f55202f809620ed0d06947dc0f295dd659ae3399
angrymusic <angrymusic@naver.com>
2023-03-28 16:31:30 +0900

    카메라 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,75 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import { Button, TouchableOpacity } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Text, View } from "../../components/Themed";
+import { Camera, CameraType } from "expo-camera";
+import { useEffect, useRef, useState } from "react";
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+    let cameraRef = useRef(null);
+    const [photo, setPhoto] = useState();
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+
+    if (!permission) {
+        // Camera permissions are still loading
+        return <View>Camera permissions are still loading</View>;
+    }
+
+    if (!permission.granted) {
+        // Camera permissions are not granted yet
+        return (
+            <View style={styles.container}>
+                <Text style={{ textAlign: "center" }}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+
+    const takePicture = async () => {
+        let options = {
+            quality: 1,
+            base64: true,
+            exif: false,
+        };
+        // if (cameraRef.current) {
+        //     let newPhoto = await cameraRef.current.takePictureAsync(options);
+        //     setPhoto(newPhoto);
+        // }
+    };
+    return (
+        <View style={styles.container}>
+            <Camera style={styles.camera} type={CameraType.back} ref={cameraRef}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={takePicture}>
+                        <Text style={styles.text}>사진 찍기</Text>
+                    </TouchableOpacity>
+                </View>
+            </Camera>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    container: {
+        flex: 1,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: "row",
+        backgroundColor: "transparent",
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: "flex-end",
+        alignItems: "center",
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "white",
+    },
 });
