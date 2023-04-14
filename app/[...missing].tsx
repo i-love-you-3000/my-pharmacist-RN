6064270b94047468b61e8e1752de2ba2da29c723
angrymusic <angrymusic@naver.com>
2023-04-14 23:47:05 +0900

    잘못된 주소 접근 시 missing 페이지

--- app/[...missing].tsx
+++ app/[...missing].tsx
@@ -8,10 +8,10 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
-        <Text style={styles.title}>This screen doesn't exist.</Text>
+        <Text style={styles.title}>잘못된 접근입니다.</Text>
 
         <Link href="/" style={styles.link}>
-          <Text style={styles.linkText}>Go to home screen!</Text>
+          <Text style={styles.linkText}>홈으로 가기</Text>
         </Link>
       </View>
     </>
